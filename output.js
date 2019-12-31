1bac4bfb401d7b32ddd1e67ebee076705027fbbc
Leonardo Toccaceli <leotoccaceli@gmail.com>
2019-12-31 18:01:44 -0300

    Fixed output.js

--- output.js
+++ output.js
@@ -1,52 +1,22 @@
 const chalk = require("chalk");
 const fs = require("fs");
 
-const { writeFile: write, readdir: read, appendFile: append } = fs;
+const { appendFile: append } = fs;
 
 function handleResponse(jokes) {
-  logResponse(jokes);
-  writeResponse(jokes);
-}
-
-function logResponse(jokes) {
   if (jokes.length === 0) {
-    console.log(chalk.red('"Aca no hacemos chistes con esas cosas"'));
+    append("joke.txt", `Aca no hacemos chistes con esas cosas`, error => {
+      if (error) console.log(error);
+      console.log(chalk.red('"Aca no hacemos chistes con esas cosas"'));
+    });
   } else {
     jokes.forEach(joke => {
-      console.log(chalk.green(`"${joke.joke}"`));
+      append("joke.txt", `${joke.joke} \n\n`, error => {
+        if (error) console.log(error);
+        console.log(chalk.green(`"${joke.joke}"`));
+      });
     });
   }
-}
-
-function writeResponse(jokes) {
-  read(__dirname, (error, files) => {
-    if (error) throw error;
-    createJokeTextFile(files, jokes);
-    appendJokesToTextFile(jokes);
-  });
-}
-
-function createJokeTextFile(files, jokes) {
-  if (!files.includes(`joke.txt`)) {
-    if (jokes.length === 0) {
-      write(`joke.txt`, `Aca no hacemos chistes con esas cosas.`, error => {
-        if (error) throw error;
-      });
-    } else {
-      write(`joke.txt`, `${jokes[0].joke} \n\n`, error => {
-        if (error) throw error;
-      });
-    }
-  }
-}
-
-function appendJokesToTextFile(jokes) {
-  let auxJokes = jokes.slice(1);
-  auxJokes.forEach(joke => {
-    append("joke.txt", `${joke.joke} \n\n`, error => {
-      console.log(error);
-    });
-  });
 }
 
 module.exports.handleResponse = handleResponse;
